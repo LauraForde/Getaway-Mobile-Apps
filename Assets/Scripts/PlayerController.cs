137d75250e86684a7f773d2cd48d43226d018c4c
LauraForde <G00315765@gmit.ie>
2017-12-13 21:20:58 +0000

    Might help if I commit things

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public float jumpForce;
+
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D rigid;
+    public bool ground;
+
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        // Get the rigid body component
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Checking if player is touching the ground
+        ground = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+        if (Input.GetKey(left)){
+            rigid.velocity = new Vector2(-speed, rigid.velocity.y);
+        }
+        else if (Input.GetKey(right))
+        {
+            rigid.velocity = new Vector2(speed, rigid.velocity.y);
+        }
+        else
+        {
+            rigid.velocity = new Vector2(0, rigid.velocity.y);
+        }
+
+        if(Input.GetKeyDown(jump) && ground)
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, jumpForce);
+        }
+
+        if(rigid.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (rigid.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        animator.SetFloat("Speed", rigid.velocity.x);
+        animator.SetBool("Grounded", ground);
+    }
+}
